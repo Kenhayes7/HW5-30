b8c46e1c799d88c0f41df1aba7347e14247413d6
Kenhayes7 <50380106+Kenhayes7@users.noreply.github.com>
2019-05-31 12:36:06 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HW5_30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            fruit F1 = new fruit("seeds", 9);
+            F1.juiceLevel(9, "seeds");
+
+            strawBerry SB1 = new strawBerry("seeds", 5, 12, "redberry");
+            SB1.ripe("ripe");
+        }
+    }
+}
---
+++ fruit.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW5_30
+{
+    public class fruit
+    {
+        public string _seeds { set; get; }
+        public int _juicy { set; get; }
+
+        public fruit(string seeds, int juicy)
+        {
+            
+            _seeds = seeds;
+            _juicy = juicy;
+            Console.WriteLine("Let's Talk about fruit");
+        }
+
+        public void juiceLevel(int juicy, string seeds)
+        {
+            Console.WriteLine("the juicy level is {0}", _juicy);
+            Console.WriteLine("this fruit has {0}", _seeds);
+        }
+    }
+
+    public class strawBerry : fruit
+    {
+        public string _name { set; get; }
+        public int _many { set; get; }
+
+        public strawBerry(string seeds, int juicy, int many) : base(seeds, juicy)
+        {
+            _seeds = seeds;
+            _juicy = juicy;
+            _name = "Mango";
+
+        }
+
+        public void ripe(string ripe)
+        {
+            Console.WriteLine("the {0} is {1}", _name, ripe);
+        }
+    }
+}
